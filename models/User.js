c3c1950582a4d4913fde134a6b116e621438bf31
K <oing10509@naver.com>
2022-08-19 17:23:32 +0900

    node set

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchma = mongoose.Schema({
+    name: {
+        type: String,
+        minlength:5
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default:0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+
+})
+
+const User = mongoose.model('user', userSchma)
+
+module.exports = { User }
